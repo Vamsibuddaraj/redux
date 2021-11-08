92a3e8e3134038dc61de2a87a7cb910b3af68358
Vamsibuddaraj <vamsibuddaraju11&gmail.com>
2021-11-08 10:41:37 +0530

    commit

--- js/firstjs.js
+++ js/firstjs.js
@@ -1,3 +1,26 @@
-function myFunction(a,b){
-    return a*b;
+const meeting=[
+
+    {
+        id:1,
+        text:'meeting was cancelled',
+        completed:true,
+    },
+    {
+        id:2,
+        text:'meeting with boss',
+        completed:true,
+    },
+    {
+        id:3,
+        text:'doc appointment',
+        completed:false,
+    },
+
+
+]
+
+console.log(meeting[2].completed)
+
+for(let i = 0;i <= 10;i++){
+    console.log(`the number is:${i}`)
 }
