af497923c81da502b28e4cc7c544a987f0bbeb7f
Vamsibuddaraj <vamsibuddaraju11&gmail.com>
2021-11-25 16:35:04 +0530

    initial commit

---
+++ REACT_REDUX/redux/src/components/Login.js
@@ -0,0 +1,29 @@
+import React from "react";
+import { useDispatch } from "react-redux";
+import { login, logout } from "../features/user";
+
+function Login() {
+  const dispatch = useDispatch();
+
+  return (
+    <div>
+      <button
+        onClick={() => {
+          dispatch(login({ name: "vamsi", age: 20, email: "varma@gmail.com" }));
+        }}
+      >
+        Login
+      </button>
+
+      <button
+        onClick={() => {
+          dispatch(logout());
+        }}
+      >
+        LOGOUT
+      </button>
+    </div>
+  );
+}
+
+export default Login;
